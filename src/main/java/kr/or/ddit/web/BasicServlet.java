3026bdbc54755d81518b254187984dda7b9a9c3c
jin <parkjinq@gmail.com>
2018-10-10 17:47:09 +0900

    second commit

--- src/main/java/kr/or/ddit/web/BasicServlet.java
+++ src/main/java/kr/or/ddit/web/BasicServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//이클립스깃 두번째 커밋
+
 @WebServlet("/BasicServlet")
 public class BasicServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
